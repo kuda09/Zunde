9eac2d5377b24820779efa26ac0c86993238678d
kuda <ku-da@hotmail.co.uk>
2016-09-03 10:56:00 +0100

    removing the incorrect directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {APP_SHELL_DIRECTIVES} from '@angular/app-shell';
 import {MdToolbar} from '@angular2-material/toolbar';
 import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
-import {APP_MENU} from '../app/app-menu/app-menu.component';
+//import {APP_MENU} from '../app/app-menu/app-menu.component';
 
 @Component({
   moduleId: module.id,
